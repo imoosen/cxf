538e1fe45cfd968d2d7a951d27d88a00bdc11189
Administrator <Administrator@PC-20160922LVPE>
2017-03-15 20:24:49 +0800

    print()

--- cxf/src/com/service/FirstWebService.java
+++ cxf/src/com/service/FirstWebService.java
@@ -5,4 +5,13 @@ import javax.jws.WebService;
 @WebService
 public class FirstWebService {
 
+	public  static void main (String[] args){
+		
+		
+	}
+	
+	public void print() {
+
+		System.out.println("wo shi print ");
+	}
 }
